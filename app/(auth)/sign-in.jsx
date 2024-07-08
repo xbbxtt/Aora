2f0d8d00e8c0fb273c2a3fa487abe841c7c3189c
Shiran <xiao.shiran@gmail.com>
2024-07-08 15:03:46 -0400

    sign in screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,13 +1,25 @@
 import { Image, ScrollView, View, Text } from 'react-native'
-import React from 'react'
+import { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
+import { Link } from 'expo-router'
+import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  })
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = () => {}
+
   return (
     <SafeAreaView className="bg-primary h-full" >
       <ScrollView contentContainerStyle={{ height: "100%" }} >
-        <View className="w-full justify-center h-full px-4 my-6">
+        <View className="w-full justify-center min-h-[70vh] px-4 my-6">
           <Image
             source={images.logo}
             className="w-[115px] h-[35px]"
@@ -16,6 +28,36 @@ const SignIn = () => {
           <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
             Log in to Aora
           </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="w-full mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Signup
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import { useFonts } from 'expo-font'
 import { useEffect } from "react";
 import { SplashScreen, Stack } from "expo-router";
@@ -37,10 +37,11 @@ const RootLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="/search/[query]" options={{ headerShown: false }} />
     </Stack>
   )
 }
 
 export default RootLayout
-
-const styles = StyleSheet.create({})
