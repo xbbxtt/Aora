ed6f643905cecfbb23e405dc8cb799495584ae6c
Shiran <xiao.shiran@gmail.com>
2024-07-08 10:54:39 -0400

    nativewind

--- app/index.jsx
+++ app/index.jsx
@@ -4,14 +4,8 @@ import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>hello.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack" >hello.</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}> Go to Profile</Link>
     </View>
